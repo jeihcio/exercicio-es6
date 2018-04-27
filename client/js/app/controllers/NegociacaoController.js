9b038ad183ab82acf72512a51f798c650dc831d5
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-04-27 12:04:28 -0300

    data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,16 +8,18 @@ class NegociacaoController {
 
 	adiciona(event) {
 		event.preventDefault();	
-
-		let data = new Date(this._inputData.value.replace(/-/g, ","));
-		console.log(data);
-
-		/*let negociacao = new Negociacao(
-			this._inputData.value,
-			this._inputQuantidade.value,
-			this._inputValor
+		let data = new Date(...
+			this._inputData.value
+				.split("-")
+				.map((item, indice) => item - indice % 2)
 		);
 
-		console.log(negociacao);*/
+		let negociacao = new Negociacao(
+			data,
+			this._inputQuantidade.value,
+			this._inputValor.value
+		);
+
+		console.log(negociacao);
 	}
 }
