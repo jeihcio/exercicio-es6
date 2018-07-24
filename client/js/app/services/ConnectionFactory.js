a12f7698ffcfd2fd7878909008eebf01a09170d1
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-07-24 14:54:52 -0300

    refatorando factory

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -12,16 +12,26 @@ class ConnectionFactory {
 			let openRequest = window.indexedDB.open(dbName, version);
 
 			openRequest.onupgradeneed = e => {
-
+				ConnectionFactory._createStores(e.target.result);		
 			}
 
 			openRequest.onsuccess = e => {
-
+				resolve(e.target.result);
 			};
 
 			openRequest.onerror = e => {
-
+				console.log(e.target.error);
+				reject(e.target.error.name);
 			}
 		});
+	}
+
+	static _createStores(connection) {
+		stores.forEach(store => {
+			if(connection.objectStoreNames.contains(store))
+				connection.deleteObjectStore(store);
+
+			connection.createObjectStore(store, { autoincrement: true });
+		});		
 	}
 }
