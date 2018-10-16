ea50bcd7867e51ad8c0478890e3354651c27b589
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-16 10:30:35 -0300

    corrigindo bug

--- client/js/app-es6/views/NegociacoesView.js
+++ client/js/app-es6/views/NegociacoesView.js
@@ -1,7 +1,7 @@
 import {View} from './View';
 import {DateHelper} from '../helpers/DateHelper';
 
-export class NegocicaoView extends View{
+export class NegociacoesView extends View{
     constructor(elemento){
         super(elemento);
     }
